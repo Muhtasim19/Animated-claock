36b8358a6aa963366df45d8fc8643648e6a3c16f
Muhtasim19 <162151530+Muhtasim19@users.noreply.github.com>
2024-08-07 09:35:18 -0400

    js update 2.2

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ setInterval(() => {
     const mm = date.getMinutes() * degree;
     const ss = date.getSeconds() * degree;
 
-    hr.style.transform = `rotateZ(${mm + (hh / 12)}deg)`;
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
     min.style.transform = `rotateZ(${mm}deg)`;
     sec.style.transform = `rotateZ(${ss}deg)`;
 
